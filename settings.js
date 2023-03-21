6dd9e975871e27ecaa2cc4bd30dc10b519eedce8
SAMIRA382 <110757901+SAMIRA382@users.noreply.github.com>
2023-03-21 14:48:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "OURGVVNpeHE=" //session Id
+global.session = "WlgzeVZpMFE=" //session Id
 global.ownernomer = "94762163767" //ur number
 global.lang = "EN" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94762163767'] //ur premium number
